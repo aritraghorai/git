8da541e3cc078281c003d684adf1919001a37c04
Aritra Ghorai <aritra1521@gmail.com>
2021-06-01 13:34:05 +0530

    sum.java modified

--- sum.java
+++ sum.java
@@ -6,6 +6,6 @@ public class sum {
     public static void main(String[] args) {
         int a=2;
         int b=3;
-        System.out.println(a+b);
+        System.out.println("The sum is :"+(a+b));
     }
 }
